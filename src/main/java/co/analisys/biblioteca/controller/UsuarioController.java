6b3362e60baa82413ff0f195927ea1299db58c4f
Luis Pinillos <pinillosluisfer@gmail.com>
2025-09-01 23:50:04 -0500

    First commit

--- src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
+++ src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import co.analisys.biblioteca.model.Usuario;
 import co.analisys.biblioteca.model.UsuarioId;
 import co.analisys.biblioteca.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +15,13 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public Usuario obtenerUsuario(@PathVariable String id) {
         return usuarioService.obtenerUsuario(new UsuarioId(id));
     }
 
     @PutMapping("/{id}/email")
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public void cambiarEmail(@PathVariable String id, @RequestBody String nuevoEmail) {
         usuarioService.cambiarEmailUsuario(new UsuarioId(id), new Email(nuevoEmail));
     }
